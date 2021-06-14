5c296e1e775a366557c446c6bccb6da57a1dad9b
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-14 19:43:38 +0300

    added error 404

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const server = express();
-// conts cors = require('cors');
-// const weatherHandler = require("./assest/weather.json");
+const cors = require('cors');
+const weatherHandler = require("./assest/weather.json");
 
 const PORT = 3010;
+
+server.use(cors());
 
 server.listen(PORT, () => {
   console.log(`listen on port ${PORT}`)
@@ -11,4 +13,22 @@ server.listen(PORT, () => {
 
 server.get('/',(req,res) =>{
     res.send('test test 1 2 1 2')
+});
+
+server.get('/weatherAll',(req,res) => {
+    res.send(weatherHandler)
+});
+
+server.get('/weather',(req,res) => {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let searchQuery = req.query.searchQuery;
+
+    let result = '';
+    if(lat == weatherHandler.lat && lon == weatherHandler.lon && searchQuery == weatherHandler.city_name){
+         result = weatherHandler.data
+    }else{
+        result = 'Not Found'
+    }
+    res.status(404).send(result)
 });
