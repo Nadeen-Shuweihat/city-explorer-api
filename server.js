a65bf704381122bf5d994a9a3a44b6e61339164d
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-14 19:50:11 +0300

    changed the line of listen

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
 const express = require("express");
 const server = express();
-const cors = require('cors');
+const cors = require("cors");
 const weatherHandler = require("./assest/weather.json");
 
 const PORT = 3010;
 
 server.use(cors());
 
+server.get("/", (req, res) => {
+  res.send("test test 1 2 1 2");
+});
+
+server.get("/weatherAll", (req, res) => {
+  res.send(weatherHandler);
+});
+
+server.get("/weather", (req, res) => {
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  let searchQuery = req.query.searchQuery;
+
+  let result = "";
+  if (
+    lat == weatherHandler.lat &&
+    lon == weatherHandler.lon &&
+    searchQuery == weatherHandler.city_name
+  ) {
+    result = weatherHandler.data;
+  } else {
+    result = "Not Found";
+  }
+  res.status(404).send(result);
+});
+
 server.listen(PORT, () => {
-  console.log(`listen on port ${PORT}`)
-});
-
-server.get('/',(req,res) =>{
-    res.send('test test 1 2 1 2')
-});
-
-server.get('/weatherAll',(req,res) => {
-    res.send(weatherHandler)
-});
-
-server.get('/weather',(req,res) => {
-    let lat = req.query.lat;
-    let lon = req.query.lon;
-    let searchQuery = req.query.searchQuery;
-
-    let result = '';
-    if(lat == weatherHandler.lat && lon == weatherHandler.lon && searchQuery == weatherHandler.city_name){
-         result = weatherHandler.data
-    }else{
-        result = 'Not Found'
-    }
-    res.status(404).send(result)
+  console.log(`listen on port ${PORT}`);
 });
