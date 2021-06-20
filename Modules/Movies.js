e9efab9377b540ed5f6846dfc80e9ce63329eef0
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-21 01:09:10 +0300

    tried to fixed

--- Modules/Movies.js
+++ Modules/Movies.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 
-class movieData {
+class MovieData {
     constructor(item){
         this.title = item.title,
         this.image = (`https://image.tmdb.org/t/p/w500${item.poster_path}`);
@@ -12,23 +12,23 @@ class movieData {
 let movieSaved = {};
 
 function MovieFunction (req,res){
-    
+    let cityName = req.query.searchQuery;
     const Key = process.env.MOVIE_KEY;
     
-    let cityName = req.query.searchQuery;
     
-    if (movieSAved[cityName] !== undefined) {
+    
+    if (movieSaved[cityName] !== undefined) {
         res.send(movieSaved[cityName]);
-        console.log("get old data");
+        console.log("get data from memory ");
       } else {
         let url = `https://api.themoviedb.org/3/search/movie?api_key=${Key}&query=${cityName}`;
         console.log("get data from API");
 
      axios.get(url)
      .then((response) => {
-        console.log(response);
+        // console.log(response);
          let result = response.data.results.map((item) => {
-          return new movieData(item);
+          return new MovieData(item);
         });
         movieSaved[cityName] = result;
         res.send(result);
--- Modules/Weather.js
+++ Modules/Weather.js
@@ -1,54 +1,45 @@
-const axios = require('axios');
-const { response } = require('express');
+const axios = require("axios");
 const weatherHandler = require("../assest/weather.json");
 
 class Data {
-    constructor(item){
-        this.valid_date = item.valid_date,
-        this.description = item.weather.description;
-    }
+  constructor(item) {
+    (this.valid_date = item.valid_date),
+      (this.description = item.weather.description);
+  }
 }
 
+function weatherOne(req, res) {
+  let result = "";
+  let searchQuery = req.query.searchQuery;
 
-function weatherOne (req, res) {
-   
-    let result = '';
-    let searchQuery = req.query.searchQuery;
-    
-    result = weatherHandler
-    .find((day) => {
-      if (searchQuery == day.city_name){
-        return day
+  result = weatherHandler
+    .find((item) => {
+      if (searchQuery == item.city_name) {
+        return item;
       }
     })
-    
-    .data.map((day) => {
-      return new Data(day)
-    })
+
+    .data.map((item) => {
+      return new Data(item);
+    });
+  res.send(result);
+}
+
+function weatherTwo(req, res) {
+  const Key = process.env.WEATHER_KEY;
+  let searchQuery = req.query.searchQuery;
+  let url = ` http://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${Key}`;
+
+  axios.get(url).then((response) => {
+    // console.log(response.data);
+
+    let result = response.data.data.map((item) => {
+      return new Data(item);
+    });
+
     res.send(result);
-   
-  
+  });
 }
 
-function weatherTwo (req,res){
-    const Key = process.env.WEATHER_KEY;
-    let searchQuery = req.query.searchQuery;
-    let url = ` http://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${Key}`;
-
-    axios.get(url)
-    .then((response) =>{
-        console.log(response.data);
-       
-        let result = response.data.data.map((item) => {
-         return new Data(item);
-        });
-
-        res.send(result);
-    })
- 
-     
-    
-}
-
-module.exports = weatherOne ;
-module.exports = weatherTwo ;
+module.exports = weatherOne;
+module.exports = weatherTwo;
